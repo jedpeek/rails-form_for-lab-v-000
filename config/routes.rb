5ff50bd601a3e3f5d527bc43afb9a7565d5130d9
Jed Peek <jedpeek@yahoo.com>
2018-08-16 06:44:57 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :students, only: [:show, :new, :create, :edit, :update]
+  resources :school_classes, only: [:index, :show, :new, :create, :edit, :update]
   resources :students, except: [:destroy, :index]
   resources :school_classes, except: [:destroy, :index]
 end
